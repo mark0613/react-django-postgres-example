1328a404c73b4b7b99825b785df057a81ac50cfd
Mark Ma <goldffd700@gmail.com>
2025-07-21 13:58:28 +0800

    style: formatting backend code

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path("api/user/", include("apps.user.urls")),
+    path('api/user/', include('apps.user.urls')),
 ]
