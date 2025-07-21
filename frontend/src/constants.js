a8d11438b8e4a5993958bda3e14e0881ebf0feb7
Mark Ma <goldffd700@gmail.com>
2025-07-21 14:10:21 +0800

    fix: 未填環境變數而 crash

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,6 +2,6 @@ import { removeTrailingSlash } from './utils/string';
 
 export const IS_DEV = import.meta.env.MODE === 'development';
 
-export const API_SERVER = IS_DEV ? '' : removeTrailingSlash(import.meta.env.VITE_API_SERVER);
+export const API_SERVER = IS_DEV ? '' : removeTrailingSlash(import.meta.env.VITE_API_SERVER || '');
 
-export const MEDIA_SERVER = IS_DEV ? '' : removeTrailingSlash(import.meta.env.VITE_MEDIA_SERVER);
+export const MEDIA_SERVER = IS_DEV ? '' : removeTrailingSlash(import.meta.env.VITE_MEDIA_SERVER || '');
